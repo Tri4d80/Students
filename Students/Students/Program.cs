009de3f55f70535815d0a4cceaeb49150f940016
Tri4d80 <bsze3simgab@vasvari.org>
2024-11-08 18:17:33 +0100

    Create New Student Object

--- Students/Students/Program.cs
+++ Students/Students/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Students;
+﻿using Students.Models;
+
+namespace Students;
 
 class Program
 {
     static void Main()
     {
+        Student adel = new Student("Kis Adél", 17);
         
+        Console.WriteLine(adel);
     }
 }
